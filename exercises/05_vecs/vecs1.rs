f0329cccecbb7110b14d95f7f70e928caae42e62
Mátyás Budavári <budavariam@gmail.com>
2026-05-19 08:49:48 +0200

    Rustlings solutions and progress

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -1,6 +1,11 @@
-fn elems_to_vec(a: i32, b: i32, c: i32) -> Vec<i32> {
-    // TODO: Return a vector containing the elements a, b and c (in this order).
-    // Use the "vec!" macro.
+fn array_and_vec() -> ([i32; 4], Vec<i32>) {
+    let a = [10, 20, 30, 40]; // Array
+    let v = vec! [10, 20, 30, 40];
+    // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
+    // Use the vector macro.
+    // let v = ???;
+
+    (a, v)
 }
 
 fn main() {
@@ -12,11 +17,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_elems_to_vec() {
-        let (a, b, c) = (2, 7, 12);
-        let v = elems_to_vec(a, b, c);
-        assert_eq!(v[0], a);
-        assert_eq!(v[1], b);
-        assert_eq!(v[2], c);
+    fn test_array_and_vec_similarity() {
+        let (a, v) = array_and_vec();
+        assert_eq!(a, *v);
     }
 }
